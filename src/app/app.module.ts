7655cecb72f57e90b1ec35a2342830abb418469c
JaimeRax <jaimecaal60@gmail.com>
2024-07-25 22:21:21 -0500

    feat(create new Module): create new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroModule } from './heroes/hero.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    HeroModule,
+    DbzModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
