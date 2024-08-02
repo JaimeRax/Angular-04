c16238ec4e7fd16e466a937e9fdafc01c913a985
JaimeRax <jaimecaal60@gmail.com>
2024-08-01 22:41:30 -0600

    feat(service): new uuid

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
   providedIn: 'root',
@@ -7,24 +8,37 @@ import { Character } from '../interfaces/character.interface';
 export class DbzService {
   public characters: Character[] = [
     {
+      id: uuid(),
       name: 'Krilin',
       power: 1000,
     },
     {
+      id: uuid(),
       name: 'Goku',
       power: 9500,
     },
     {
+      id: uuid(),
       name: 'Vegeta',
       power: 9300,
     },
   ];
 
   onNewCharacter(character: Character): void {
+    const newCharacter: Character = {
+      id: uuid(),
+      ...character,
+    };
+
     this.characters.push(character);
   }
 
-  onDeleteCharacter(index: number) {
-    this.characters.splice(index, 1);
+  // onDeleteCharacter(index: number) {
+  //   this.characters.splice(index, 1);
+  // }
+  deleteCharacterById(id: string) {
+    this.characters = this.characters.filter(
+      (character) => character.id !== id,
+    );
   }
 }
